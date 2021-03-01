821bb99b870bbadd2c60980036be9c3a6cb90ec2
retrohead <retro_head@hotmail.com>
2021-03-01 04:31:03 +0000

    0.1

---
+++ SatitorRingsConfig/frmMain.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace SatiatorRingsConfig
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.txtDir = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.txtImageID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.btnBuild = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtDir
+            // 
+            this.txtDir.Location = new System.Drawing.Point(11, 11);
+            this.txtDir.Name = "txtDir";
+            this.txtDir.ReadOnly = true;
+            this.txtDir.Size = new System.Drawing.Size(531, 20);
+            this.txtDir.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBrowse.Location = new System.Drawing.Point(548, 9);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Open Folder";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.BtnBrowse_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(11, 37);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(316, 483);
+            this.treeView1.TabIndex = 2;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.TreeView1_AfterSelect);
+            // 
+            // txtImageID
+            // 
+            this.txtImageID.Location = new System.Drawing.Point(401, 37);
+            this.txtImageID.Name = "txtImageID";
+            this.txtImageID.ReadOnly = true;
+            this.txtImageID.Size = new System.Drawing.Size(100, 20);
+            this.txtImageID.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(345, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Image ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(358, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Image";
+            // 
+            // btnApply
+            // 
+            this.btnApply.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnApply.Location = new System.Drawing.Point(401, 175);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(100, 27);
+            this.btnApply.TabIndex = 7;
+            this.btnApply.Text = "Apply New Image";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.BtnApply_Click);
+            // 
+            // btnBuild
+            // 
+            this.btnBuild.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBuild.Enabled = false;
+            this.btnBuild.Location = new System.Drawing.Point(401, 462);
+            this.btnBuild.Name = "btnBuild";
+            this.btnBuild.Size = new System.Drawing.Size(222, 58);
+            this.btnBuild.TabIndex = 8;
+            this.btnBuild.Text = "Build ISO";
+            this.btnBuild.UseVisualStyleBackColor = true;
+            this.btnBuild.Click += new System.EventHandler(this.BtnBuild_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::SatiatorRingsConfig.Properties.Resources.satiator_logo;
+            this.pictureBox2.Location = new System.Drawing.Point(471, 343);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(150, 113);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(401, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 528);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnBuild);
+            this.Controls.Add(this.btnApply);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtImageID);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtDir);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Satiator Rings Configuation";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDir;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox txtImageID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.Button btnBuild;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ SatitorRingsConfig/frmMain.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SatiatorRingsConfig
+{
+    public partial class frmMain : Form
+    {
+        private class nodeData
+        {
+            public string fn;
+            public int imageId;
+        }
+        TGA T;
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBrowse_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    txtDir.Text = fbd.SelectedPath;
+                    string[] dirs = Directory.GetDirectories(fbd.SelectedPath);
+                    foreach (string dir in dirs)
+                    {
+                        TreeNode node = new TreeNode();
+                        nodeData data = new nodeData();
+                        data.fn = dir;
+                        string fn = dir.Replace(txtDir.Text, "");
+                        fn = fn.Substring(1, fn.Length - 1);
+                        data.imageId = -1;
+                        if (fn.EndsWith("]"))
+                        {
+                            string idStr = fn.Substring(fn.LastIndexOf(" [") + 2, fn.Length - (fn.LastIndexOf(" [") + 2) - 1);
+                            data.imageId = int.Parse(idStr);
+                            fn = fn.Substring(0, fn.LastIndexOf(" ["));
+                        }
+                        node.Text = fn;
+                        node.Tag = data;
+                        treeView1.Nodes.Add(node);
+                    }
+                }
+            }
+        }
+
+        private void TreeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            nodeData data = (nodeData)e.Node.Tag;
+            txtImageID.Text = data.imageId.ToString();
+
+            if (txtImageID.Text == "-1")
+                return;
+            string path = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "iso", "cd", "BOX");
+            path = Path.Combine(path, txtImageID.Text + "S.TGA");
+            if (!File.Exists(path))
+            {
+                pictureBox1.Visible = false;
+                return;
+            }
+            T = new TGA(path);
+            pictureBox1.Image =(Bitmap)T;
+            pictureBox1.Visible = true;
+        }
+
+        private void BtnBuild_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            if (File.Exists("satiator-rings.iso"))
+                File.Delete("satiator-rings.iso");
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = @"iso\mkisofs.exe";
+            startInfo.Arguments = "-quiet -sysid \"SEGA SATURN\" -volid \"SaturnApp\" -volset \"SaturnApp\" -sectype 2352 -publisher \"SEGA ENTERPRISES, LTD.\" -preparer \"SEGA ENTERPRISES, LTD.\" -appid \"SaturnApp\" -abstract \"./iso/cd/ABS.TXT\" -copyright \"./iso/cd/CPY.TXT\" -biblio \"./iso/cd/BIB.TXT\" -generic-boot \"./iso/IP.BIN\" -full-iso9660-filenames -o satiator-rings.iso ./iso/cd";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        int freeBoxartID()
+        {
+            for(int i=0;i<100;i++)
+            {
+                string path = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "iso", "cd", "BOX");
+                path = Path.Combine(path, i + "S.TGA");
+                if (!File.Exists(path))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private void BtnApply_Click(object sender, EventArgs e)
+        {
+            nodeData data = (nodeData)treeView1.SelectedNode.Tag;
+            int id = freeBoxartID();
+            if(id < 0)
+            {
+                MessageBox.Show("Maximum boxarts reached", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "PNG (*.png)|*.png|JPG (*.jpg)|*.jpg";
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                using (Image img = Image.FromFile(fd.FileName))
+                {
+                    Image newimg;
+                    double imgratio = (double)img.Width / (double)img.Height;
+                    if (imgratio > 0.8)
+                    {
+                        // presume japanese
+                        newimg = ResizeImage(img, 80, 80);
+                    }
+                    else
+                    {
+                        // presume eur / usa
+                        newimg = ResizeImage(img, 64, 100);
+                    }
+                    newimg.Save("tmp.png");
+                    newimg.Dispose();
+
+                    using (Bitmap original = new Bitmap("tmp.png"))
+                    using (Bitmap clone = new Bitmap(original))
+                    using (Bitmap newbmp = clone.Clone(new Rectangle(0, 0, clone.Width, clone.Height), PixelFormat.Format8bppIndexed))
+                        T = (TGA)newbmp;
+                    string path = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "iso", "cd", "BOX");
+                    path = Path.Combine(path, id + "S.TGA");
+                    T.Save(path);
+                    pictureBox1.Image = (Bitmap)T;
+                    pictureBox1.Visible = true;
+
+                    string newDirName = treeView1.SelectedNode.Text;
+                    if(newDirName.LastIndexOf('/') > -1)
+                    {
+                        newDirName = newDirName.Substring(0, newDirName.LastIndexOf(" ["));
+                    }
+                    newDirName  = newDirName + " [" + id + "]";
+                    newDirName = Path.Combine(Path.GetDirectoryName(data.fn), newDirName);
+                    treeView1.SelectedNode.Tag = data;
+                    if(data.fn != newDirName)
+                        Directory.Move(data.fn, newDirName);
+                    data.fn = newDirName;
+                    data.imageId = id;
+                }
+            }
+        }
+
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+    }
+}
